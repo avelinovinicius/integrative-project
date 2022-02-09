b3d0fbf054c86d4e49eefcc4aefe0d47674a2f61
avelinovinicius <vinicius.avelino@mercadolivre.com>
2022-02-09 11:06:23 -0300

    Refactoring

--- src/test/java/com/meli/bootcamp/integrativeproject/integration/ProductControllerIT.java
+++ src/test/java/com/meli/bootcamp/integrativeproject/integration/ProductControllerIT.java
@@ -4,7 +4,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -22,9 +21,6 @@ public class ProductControllerIT {
 
     @Autowired
     private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
 
     @Test
     public void findAllPagedShouldReturnSortedPageWhenSortByName() throws Exception
